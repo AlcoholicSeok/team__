a41fa82e34b0167acee381e34cf96feb6bbd48fe
AlcoholicSeok <102846496+AlcoholicSeok@users.noreply.github.com>
2022-05-21 23:46:05 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,13 @@
+#include <filesystem>
+#include <iostream>
+
+int main() {
+    filesystem::path p("./some_file");
+
+    std::cout << "Does " << p << " exist? [" << std::boolalpha
+        << std::filesystem::exists(p) << "]" << std::endl;
+    std::cout << "Is " << p << " file? [" << std::filesystem::is_regular_file(p)
+        << "]" << std::endl;
+    std::cout << "Is " << p << " directory? [" << std::filesystem::is_directory(p)
+        << "]" << std::endl;
+}
